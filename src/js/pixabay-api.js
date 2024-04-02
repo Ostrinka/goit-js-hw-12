d1a6300a5b51c64ffdc158377576a13097d24ab5
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-04-02 15:34:51 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,19 @@
 import axios from 'axios';
 
-import { inputUser } from '../main';
+import { inputUser, currentPage } from '../main';
 
 export let amountOfHits = 0;
 
 export async function getImage() {
   try {
-    const response = await axios.get(`https://pixabay.com/api/?key=43028681-4db453fe74ec5ab2aaa5007ef&q=${inputUser}&image_type=photo&page=currentPage&per_page=15&orientation=horizontal&safesearch=true`);
+    const response = await axios.get(`https://pixabay.com/api/?key=43028681-4db453fe74ec5ab2aaa5007ef&q=${inputUser}&image_type=photo&page=${currentPage}&per_page=15&orientation=horizontal&safesearch=true`);
 
-    const totalHits = response.data.hits;
-    amountOfHits = totalHits;
-    return totalHits;
+    const images = response.data.hits;
+    amountOfHits = images.length;
+    return images;
   } catch (error) {
-      console.error(error);
+    console.error(error);
   }
 }
-
 
 
